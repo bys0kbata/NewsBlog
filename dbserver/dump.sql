fa843341fa33f619f5d219333250cca8a9b8b562
bys0kbata <92207641+bys0kbata@users.noreply.github.com>
2024-04-05 00:24:25 +0400

    Update dump.sql

--- dbserver/dump.sql
+++ dbserver/dump.sql
@@ -25,22 +25,69 @@ SET default_table_access_method = heap;
 --
 
 CREATE TABLE public."News" (
-    idnews bigint NOT NULL,
+    idnews integer NOT NULL,
     namenews character varying NOT NULL,
     textnews character varying NOT NULL,
-    datapublish date NOT NULL
+    datepublish character varying NOT NULL
 );
 
 
 ALTER TABLE public."News" OWNER TO postgres;
 
 --
+-- Name: News_idnews_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public."News_idnews_seq"
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public."News_idnews_seq" OWNER TO postgres;
+
+--
+-- Name: News_idnews_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public."News_idnews_seq" OWNED BY public."News".idnews;
+
+
+--
+-- Name: News idnews; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."News" ALTER COLUMN idnews SET DEFAULT nextval('public."News_idnews_seq"'::regclass);
+
+
+--
 -- Data for Name: News; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public."News" (idnews, namenews, textnews, datapublish) FROM stdin;
-1	ПГУТИ	Привет	2005-01-28
-\.
+INSERT INTO public."News" VALUES (7, 'PSUTICOOL', 'COOL OSANOV', '1.1.2024');
+INSERT INTO public."News" VALUES (8, 'PSUTICOOL', 'COOL OSANOV', '1.1.2024');
+INSERT INTO public."News" VALUES (9, 'PSUTICOOL', 'COOL OSANOV', '1.1.2024');
+INSERT INTO public."News" VALUES (10, 'PSUTICOOL', 'COOL OSANOV', '1.1.2024');
+INSERT INTO public."News" VALUES (11, 'PSUTICOOL', '', 'undefined');
+INSERT INTO public."News" VALUES (12, '', 'undefined', 'undefined');
+INSERT INTO public."News" VALUES (13, 'undefined', 'undefined', 'undefined');
+INSERT INTO public."News" VALUES (14, 'undefined', 'undefined', 'undefined');
+INSERT INTO public."News" VALUES (15, 'undefined', 'undefined', 'undefined');
+INSERT INTO public."News" VALUES (16, 'null', 'null', 'null');
+INSERT INTO public."News" VALUES (17, 'null', 'null', 'null');
+INSERT INTO public."News" VALUES (18, '', '', '');
+INSERT INTO public."News" VALUES (19, 'NULL', 'NULL', 'NULL');
+INSERT INTO public."News" VALUES (20, 'sfd', 'sdf', '33.34.34');
+
+
+--
+-- Name: News_idnews_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public."News_idnews_seq"', 20, true);
 
 
 --
